e86b188767d3f09119d4c2e157d2fbc6048888c4
Maynor <1925597@clases.edu.sv>
2025-03-19 07:59:56 -0600

    Agregar archivos de proyecto.

---
+++ MABO20250319.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MABO20250319.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
